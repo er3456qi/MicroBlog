1517e07b12f659bf911f0549348c7640767ee6c3
er3456qi <er3456qi@outlook.com>
2016-05-12 22:27:52 +0800

    reconfig gitignore

---
+++ app/forms.py
@@ -0,0 +1,11 @@
+from flask_wtf import Form
+from wtforms import StringField, BooleanField
+from wtforms.validators import DataRequired
+
+
+class LoginForm(Form):
+    # DataRequired 验证器只是简单地检查相应域提交的数据是否是空。
+    openid = StringField('openid', validators=[DataRequired()])
+    remember_me = BooleanField('remember_me', default=False)
+
+
