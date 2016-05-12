2d3bdfc87c944e87cd036bff859ec46133a6ff8b
er3456qi <er3456qi@outlook.com>
2016-05-12 21:06:17 +0800

    add template

--- app/views.py
+++ app/views.py
@@ -1,7 +1,20 @@
 from app import app
+from flask import render_template
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'hello world'
+    title = 'Hi there'
+    user = {'nickname': 'Wnbot' }
+    posts = [
+        {
+        'author': {'nickname': 'John'},
+        'body': 'Beautiful day in Portland!'
+        },
+        {
+         'author': { 'nickname': 'Susan' },
+         'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title=title, user=user, posts=posts)
